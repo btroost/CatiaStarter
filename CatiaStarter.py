508c3b3d8fc9cc2a05bb7af0493d0ac0f967d6d0
btroost <59251634+btroost@users.noreply.github.com>
2022-07-05 21:55:30 +0200

    version 0.1 to test on windows

--- CatiaStarter.py
+++ CatiaStarter.py
@@ -1,5 +1,6 @@
 #CatiaStarter
 import tkinter as tk
+from tkinter import ttk
 import os
 import subprocess
 #import xml.dom.minidom
@@ -9,18 +10,7 @@ import xml.etree.ElementTree as ET
 # description
 ######################################################################
 
-#  Variables
-OS="mac"        # choose mac or win
-if OS=="mac":
-    Settingsdir = "/Users/berttroost/develop/GitHub/CatiaStarter/"
-    Workingdir = "/Users/berttroost/develop/GitHub/CatiaStarter/"
-if OS =="win":
-    Settingsdir = "/Users/berttroost/develop/GitHub/CatiaStarter/"
-    Workingdir = "/Users/berttroost/develop/GitHub/CatiaStarter/"
 
-#import os
-#os.path.join('app', 'subdir', 'dir', 'filename.foo')
-#'app/subdir/dir/filename.foo'
 
 def getsettingsdata():
     print("getsettingsdata")
@@ -39,6 +29,7 @@ def getsettingsdata():
     for program in root.findall("./programs/program"):
         Programs.append(program.get('name'))
     Settings.append(Programs)
+    program="insettingsdata"
 
     root = ET.parse(Settingsdir + 'licenserole.xml').getroot()
     for program in root.findall("./licenseroles/role"):
@@ -46,9 +37,22 @@ def getsettingsdata():
     Settings.append(LicenseRole)
     return Settings
 
+def ddProgramAction(choice):
+    choice = proglist.get()
+    print(choice)
 
+def ddUsageAction(choice):
+    choice = usagelist.get()
+    print(choice)
+
+def ddLicenseAction(choice):
+    choice = licenselist.get()
+    print(choice)
 
 def GUI(settings):
+    global window
+    program="tbd"
+
     window=tk.Tk()
     window.title(" Catia Starter ")
     window.geometry("600x400")
@@ -56,34 +60,70 @@ def GUI(settings):
     # Usage Scope
     newlabel = tk.Label(text = " Select your Usagescope ")
     newlabel.grid(column=0,row=0)
-    #    dropdown1=tk.OptionMenu(window,tk.IntVar(),"Papendrecht","Hoogerheide","Helmond","Filton")
-    dropdown1=tk.OptionMenu(window,tk.IntVar(),settings[0][0],settings[0][1],settings[0][2])
-    dropdown1.grid(column = 1, row=0)
+
+    usagescope = []
+    for usage in settings[0]:
+        usagescope.append(usage)
+    global usagelist
+    usagelist = tk.StringVar()
+    if len(usagescope) > 0 :
+        usagelist.set(usagescope[0])
+    dropdown = tk.OptionMenu(
+        window,
+        usagelist,
+        *usagescope,
+        command=ddUsageAction
+    )
+    dropdown.grid(column = 1, row=0)
 
     # Program
     newlabel = tk.Label(text = " Select your Program ")
     newlabel.grid(column=0,row=1)
-    dropdown=tk.OptionMenu(window,tk.IntVar(),settings[1][0],settings[1][1])
+    programs = []
+    for progs in settings[1]:
+        programs.append(progs)
+    global proglist
+    proglist = tk.StringVar()
+    if len(programs) > 0 :
+        proglist.set(programs[0])
+    dropdown = tk.OptionMenu(
+        window,
+        proglist,
+        *programs,
+        command=ddProgramAction
+    )
     dropdown.grid(column = 1, row=1)
 
     # LicenseRole
     newlabel = tk.Label(text = " Select your LicenseRole ")
     newlabel.grid(column=0,row=2)
-    dropdown=tk.OptionMenu(window,tk.IntVar(),settings[2][0],settings[2][1])
-    dropdown.grid(column = 1, row=2)
 
-    # Button Start Catia
-    mybutton = tk.Button(window, text = "Start Catia")
-    mybutton.grid(column=1,row=3)
-    mybutton.bind("<Button-1>",start)
+    license = []
+    for licrole in settings[2]:
+        license.append(licrole)
+    global licenselist
+    licenselist = tk.StringVar()
+    if len(license) > 0 :
+        licenselist.set(license[0])
+    dropdown = tk.OptionMenu(
+        window,
+        licenselist,
+        *license,
+        command=ddLicenseAction
+    )
+    dropdown.grid(column = 1, row=2)
 
     # Button Check licenses
     mybutton = tk.Button(window, text = "Check License")
-    mybutton.grid(column=0,row=4)
+    mybutton.grid(column=0,row=3)
     mybutton.bind("<Button-1>",CheckLicense)
 
-    window.mainloop()
+    # Button Start Catia
+    mybutton = tk.Button(window, text = "Start Catia")
+    mybutton.grid(column=1,row=4)
+    mybutton.bind("<Button-1>",Start)
 
+    window.mainloop()
 
 def CheckLicense(event):
     # 1) check license
@@ -94,56 +134,62 @@ def CheckLicense(event):
 
     arg1=r"C:\Program Files\Dassault Systemes\DS License Server\win_b64\code\bin\DSLicSrv.exe"
     arg2=r"c:\data\apps\startup\dslsinput.txt"
-    arg3=r"c:\data\apps\startup\dslsoutputpy.txt"
-    #result = subprocess.Popen([arg1, "-admin", "-i", arg2, "-o", arg3])    print (result)
+#    arg3=r"c:\data\apps\startup\dslsoutputpy.txt"
+    myworkdir = my_env["TEMP"]
+    #my_env.get("TEMP", '') in case temp does not exist..
+    arg3=myworkdir + "\\dslsoutputpy.txt"
+    result = subprocess.Popen([arg1, "-admin", "-i", arg2, "-o", arg3])
+    print (result)
 
     f = open(arg3, "r")
     for x in f:
         print(x)
 
-def start(event):
-    print(" Start Catia")
-
-    program = "Airbus"
+def Start(event):
+#    print(" Start Catia")
+    program=proglist.get()
+#    print (program)
 
     #Get variables to start Catia
+    envdir=""
     root = ET.parse(Settingsdir + 'programs.xml').getroot()
     for program in root.findall("./programs/program[@name='"+program+"']"):
         for variable in program.findall("./variables/variable"):
-#            print(variable.find('name').text)
-            print(variable.find('value').text)
+            if variable.find('name').text=="envdir":
+                envdir = variable.find('value').text
+            #print(variable.find('name').text)
+            #print(variable.find('value').text)
+        print(envdir)
 
         # set environment Variables
         my_env = os.environ.copy()
         for variable in program.findall("./envvariables/variable"):
-            my_env[variable.find('name').text] = variable.find('value').text
-#            print("test")
+            my_env[variable.find('name').text.upper()] = variable.find('value').text
+            #print (variable.find('name').text.upper())
 
-    #my_env["PATH"] = "/usr/sbin:/sbin:" + my_env["PATH"]
-#    my_env["testvar"] = "MyTestVarValues"
-##    my_command = "export > tstenv.txt"
-#    my_command = "c:\data\apps\testenv.cmd"
-#    subprocess.Popen(my_command, env=my_env)
+    StartCommand="C:\\Program Files\\Dassault Systemes\\B30\\win_b64\\code\\bin\\CATSTART.exe"
+    Arg1="-nowindow"
+    Arg2= envdir
+    Arg3=""
 
-    #os.environ['SOMEVAR'] = 'SOMEVAL'
-    #you might use my_env.get("PATH", '') instead of my_env["PATH"] in case PATH somehow not defined in the original environment, but other than that it looks fine.
+    result = subprocess.Popen([StartCommand, Arg1, Arg2, Arg3], env=my_env)
+    print(result)
+    ##window.destroy()
 
-    #Start-Process  -WorkingDirectory "C:\Program Files\Dassault Systemes\B30\win_b64\code\bin" -ArgumentList "-nowindow"  #-nowindow  -ArgumentList "/c dir `"%systemdrive%\program files`""
-    #os.system(""C:\Program Files\Dassault Systemes\B30\win_b64\code\bin\CATSTART.exe"")
-    # "-nowindow"  #-nowindow  -ArgumentList "/c dir `"%systemdrive%\program files`""
-
-    #file = "C:/Program Files/Dassault Systemes/B30/win_b64/code/bin/CATSTART.exe"
-    #subprocess.call([file])
-
-###    result = subprocess.Popen(["C:\Program Files\Dassault Systemes\DS License Server\win_b64\\code\\bin\\DSLicSrv.exe", "-admin", "-i", "c:\data\\apps\startup\dslsinput.txt", "-o", "c:\data\\apps\startup\dslsoutputpy.txt"])
- ###  result = subprocess.Popen(["C:\Program Files\Dassault Systemes\DS License Server\win_b64\\code\\bin\\DSLicSrv.exe", "-admin", "-i", "c:\data\\apps\startup\dslsinput.txt", "-o", "c:\data\\apps\startup\dslsoutputpy.txt"], env=my_env)
-
-#    print (result)
 
 #*************Start ProGRAM *********************************
-# get settingsdata
+
+#  Set Variables
+OS="mac"        # choose mac or win
+if OS=="mac":
+    Settingsdir = "/Users/berttroost/develop/GitHub/CatiaStarter/"
+    Workingdir = "/Users/berttroost/develop/GitHub/CatiaStarter/"
+if OS =="win":
+    Settingsdir = "C:\\data\\CatiaStarter\\"
+    Workingdir = "C:\\data\\CatiaStarter\\"
+Program = "none"
+
+# Get settingsdata
 settings=getsettingsdata()
-#for row in settings:
-#    print(row)
-#Create Graphical User Interface
+# Create Graphical User Interface
 GUI(settings)
